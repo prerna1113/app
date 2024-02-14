31faf4b87ab55e3ba4031af70ef71b1a56e44719
Pre-11 <prernakumari7427@gmail.com>
2024-02-14 22:52:19 +0530

    method added

---
+++ src/component/obj.js
@@ -0,0 +1,22 @@
+
+
+//creating obj using the object literal syntax 
+
+var p1 = {name :'Steve'};
+
+// creating obj using the object() constructor function
+
+var p2 = new Object();
+p2.name = 'Steve';
+
+var person ={
+    firstName :'John',
+    lastName:'Smith',
+    age:'16',
+    getFullName : function(){
+        return console.log("Hello " + this.firstName + ' ' + this.lastName);
+    }
+}
+
+person.getFullName();
+console.log(p2.name);

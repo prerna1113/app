24ba83f73bb097d038f3669ffba8ac6bdb08bc97
Pre-11 <prernakumari7427@gmail.com>
2024-02-17 20:38:12 +0530

    added

--- src/revision/Classes.js
+++ src/revision/Classes.js
@@ -41,3 +41,31 @@ var func = new MyFunc();
 func.name ="Prerna";
 func.age = 23;
 console.log(func.getFullName())
+
+//retreive properties of an object
+console.log(Object.keys(func));
+
+//property Descriptor 
+console.log(Object.getOwnPropertyDescriptor(func,'name'));
+
+
+//Object.defineProperty
+function Student(){
+    this.name ="steve";
+    this.gender="male";
+}
+var student1 = new Student();
+Object.defineProperty(student1,'name',{Enumerable:false})
+
+for(var prop in student1){
+    console.log(prop);
+}
+
+try{
+    student1.name  ="james";
+    console.log(student1.name);
+}
+catch(ex){
+    console.log(ex.message);
+}
+

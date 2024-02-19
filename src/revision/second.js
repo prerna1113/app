7b75bb0e19a2a1f541d8e18522bb4799697978cb
Pre-11 <prernakumari7427@gmail.com>
2024-02-19 22:42:56 +0530

    method added

--- src/revision/second.js
+++ src/revision/second.js
@@ -31,3 +31,43 @@ console.log(Person.ShowLove());
 for (var prop in Person){
     console.log(prop)
 }
+
+//Classes 
+//so ecmascript doesn't have classes type but we can create function in js in such a way that
+//it will act as a class
+
+// function can be used like a class in js
+
+function Person1(){
+    this.name ="Unknown";
+    this.age="Unknown";
+    this.gender="Unknown";
+}
+
+var Person2 = new Person1();
+Person2.name ="Faizan";
+Person2.age ="26";
+Person2.gender="Male";
+
+console.log(Person2.name,Person2.age,Person2.gender);
+
+
+//add methods in a class
+
+function AddMethod(){
+    this.name ="unknown";
+    this.add ="unknown";
+    this.showFunction = function(){
+        return this.name +" "+this.add;
+    }
+}
+
+var addMethod = new AddMethod();
+addMethod.name = "class";
+addMethod.add = "Method";
+
+console.log(addMethod.showFunction());
+
+
+
+

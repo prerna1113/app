27c86b8fdf0c62c773a4727d2a7b51f1482ad1df
Pre-11 <prernakumari7427@gmail.com>
2024-02-20 21:28:07 +0530

    added

--- src/Js Topics/Prototype.js
+++ src/Js Topics/Prototype.js
@@ -10,12 +10,34 @@ function Student(){
 var studentobj1 = new Student();
 studentobj1.age = 15;
 console.log(studentobj1.name);
-
+ 
 var studentobj2 = new Student();
 console.log(studentobj2.gender);
 console.log(studentobj2.age);     // undefined
 
 // age property is attached to studentObj1 instance 
+//however studobj2 instance will not have  the age property because
+//it is defined only on studentObj1 instance.
+//so we can use prototype if we want to add new properties at later stage of a function which
+//will be shared across all the instances.
+//Prototype is an object that is associated with every functions and objects by default 
+//in js where function's prototype is accessible & modifiable and object's prototype 
+//property is not visible 
+//every functon includes prototype object by default
+//the prototype object is special type of enumerable object to which additional properties can be 
+//attached to it which will be shared across all the instances of it's constructor function
+
+
+function student(){
+    this.name = 'john';
+    this.gender = 'M';
+}
+student.prototype.age = 15;
+var studObj1 = new student();
+console.log(studObj1.age);
+
+var studObj2 = new student();
+console.log(studObj2.age);
 
 
 
